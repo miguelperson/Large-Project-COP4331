965f1c2e8696d37c9b408be568944bb9317bc75f
Miguel Baca-Urteaga <miguelbaca@Knights.ucf.edu>
2023-06-23 14:24:52 -0400

    added express import

--- APIFolder/LoginAPI.js
+++ APIFolder/LoginAPI.js
@@ -2,3 +2,4 @@
 // initial log in page
 var http = require('http'); // allows for http requesting
 var mongo = require('mongodb'); // imports library for database manipulation
+const express = require('express');
