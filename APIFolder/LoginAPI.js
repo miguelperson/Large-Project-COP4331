a74102376f0818d5cd4bc757617f2d6b8707f125
Miguel Baca-Urteaga <miguelbaca@Knights.ucf.edu>
2023-06-23 14:21:46 -0400

    Creating the login api program

---
+++ APIFolder/LoginAPI.js
@@ -0,0 +1,4 @@
+// JavaScript source code
+// initial log in page
+var http = require('http'); // allows for http requesting
+var mongo = require('mongodb'); // imports library for database manipulation
