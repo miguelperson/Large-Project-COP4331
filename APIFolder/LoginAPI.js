66f4c35aec92d16c4410eff84eb45e4d5a0d1bd5
nstuhldreher1 <92557643+nstuhldreher1@users.noreply.github.com>
2023-06-27 21:33:44 -0400

    Delete APIFolder directory

--- APIFolder/LoginAPI.js
+++
@@ -1,5 +0,0 @@
-// JavaScript source code
-// initial log in page
-var http = require('http'); // allows for http requesting
-var mongo = require('mongodb'); // imports library for database manipulation
-const express = require('express');
